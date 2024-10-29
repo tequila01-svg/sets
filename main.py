ea131468340a0050bf52b5c46b16091510525f03
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-29 18:21:06 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+my_set={1,2,3,4,5,6,7,8,9}
+print(my_set)
+#adding element
+my_set.add(10)
+print("updated set", my_set)
+my_set2={1,2,3,4,5}
+my_set3=my_set.intersection(my_set2)
+print(my_set3)
+my_set4=my_set.union(my_set2)
+print(my_set4)
